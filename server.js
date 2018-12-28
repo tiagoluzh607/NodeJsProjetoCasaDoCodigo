98775d57c488b0d87e95d179644182b6f60d7922
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-12-28 10:49:48 -0200

    Encapsulando a Configuracao do Express

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const app = express();
+const app = require('./src/config/custom-express');
 
 app.listen(3000, function(){
     console.log(`Servidor Rodando na Porta 3000`);
